a9653eb83389d64ded5caae7bb8c1770a2b5ef1d
Sergio Guidi <serjaou@gmail.com>
2020-02-10 18:13:42 -0400

    Fix layout styles and add a content element

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
 import './App.css';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <div className='content'>Hello World</div>
       <Footer />
     </div>
   );
