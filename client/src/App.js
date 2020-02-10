58f7696c6374bc3f884e937a6bd78d92a21d92ed
Sergio Guidi <serjaou@gmail.com>
2020-02-10 16:46:27 -0400

    Remove crap skeleton code

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+  return <h1>Hello</h1>;
 }
 
 export default App;
