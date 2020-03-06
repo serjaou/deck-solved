6dbfe55ee4f31d1d4640e2193339363ece707e7a
Sergio Guidi <serjaou@gmail.com>
2020-03-05 20:04:58 -0400

    Isolate server files into their self folder

---
+++ server/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cards = require('./api/cards');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Connect to the database
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB connected.'))
+  .catch(error => console.log(error));
+
+// Use cards API
+app.use('/api/cards', cards);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}.`));
