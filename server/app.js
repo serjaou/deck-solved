c5349006c460dba5b1e2c5e45db8d868661c814f
Sergio Guidi <serjaou@gmail.com>
2020-03-22 19:57:37 -0400

    Add some scripts to package.json to make production work

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cards = require('./api/cards');
 const rulings = require('./api/rulings');
 
@@ -21,6 +22,16 @@ mongoose
 // Use cards API
 app.use('/api/cards', cards);
 app.use('/api/rulings', rulings);
+
+// Serve static assets on production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
