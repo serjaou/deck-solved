934b6681469ac2d7ad11fdc078ed56f5fe307f74
Sergio Guidi <serjaou@gmail.com>
2020-03-28 10:26:22 -0400

    Refactor ResultsImages to improve its reactiveness

--- client/src/components/search/results/ResultsImages.js
+++ client/src/components/search/results/ResultsImages.js
@@ -5,21 +5,24 @@ import { useHistory } from 'react-router-dom';
 import { CardImage } from '../../card';
 
 const useStyles = makeStyles({
-  card: {
-    maxWidth: '19rem',
-    width: matches => (matches ? '100%' : '19rem')
-  },
-  container: {
-    justifyContent: 'center'
-  }
+  // dynamic card-classes for grid responsiveness.
+  small: { width: '98.5%', minWidth: '14rem' },
+  medium: { width: '49.25%', minWidth: '14rem' },
+  large: { width: '32.75%', minWidth: '14rem' },
+  default: { width: '24.5%', minWidth: '14rem' }
 });
 
 function ResultsImages(props) {
-  const matches = useMediaQuery('(max-width:600px)');
-  const classes = useStyles(matches);
+  const classes = useStyles();
   const history = useHistory();
 
-  const handleClick = card => {
+  // view-port media queries.
+  const small = useMediaQuery('(max-width:512px)');
+  const medium = useMediaQuery('(max-width:768px)');
+  const large = useMediaQuery('(max-width:1024px)');
+  const cardClass = small ? 'small' : medium ? 'medium' : large ? 'large' : 'default';
+
+  const gotoCardPage = card => {
     history.push({
       pathname: `/cards/${encodeURIComponent(card.name)}`,
       state: { card }
@@ -27,10 +30,10 @@ function ResultsImages(props) {
   };
 
   return (
-    <Grid container className={classes.container} direction='row' spacing={1}>
-      {props.cards.map(card => (
-        <Grid className={classes.card} key={card.id} item>
-          <CardImage onClickHandler={() => handleClick(card)} card={card} variant='normal' />
+    <Grid container direction='row' spacing={1}>
+      {props.paginatedData.data.map(card => (
+        <Grid className={classes[cardClass]} key={card.id} item>
+          <CardImage onClickHandler={() => gotoCardPage(card)} card={card} variant='normal' />
         </Grid>
       ))}
     </Grid>
