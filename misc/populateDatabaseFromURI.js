b196d5bba64bdbfa32f4bea2cc7eabfb87b052f7
Sergio Guidi <serjaou@gmail.com>
2020-03-29 18:04:54 -0400

    Fix populateDatabaseFromURI script to populate playable cards only

--- misc/populateDatabaseFromURI.js
+++ misc/populateDatabaseFromURI.js
@@ -1,125 +1,147 @@
-require('dotenv').config('');
+require('dotenv').config();
 const mongoose = require('mongoose');
-const cardSchema = require('../schemas/cardSchema');
+const cardSchema = require('../models/schemas/cardSchema');
 
-async function connectToDatabases(sourceURI, targetURI) {
-  let sourceModel = {};
-  let targetModel = {};
-
+async function populateDatabaseFromURI(sourceURI, targetURI) {
   try {
+    //
+    // connect to MongoDB databases.
+    //
+    console.log('Connecting to MongoDB databases...');
     const sourceConn = await mongoose.createConnection(sourceURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    sourceModel = sourceConn.model('Card', cardSchema);
-  } catch (error) {
-    console.log(error);
-  }
-  console.log('Source MongoDB connected.');
+    const sourceModel = sourceConn.model('Card', cardSchema);
 
-  try {
     const targetConn = await mongoose.createConnection(targetURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    targetModel = targetConn.model('Card', cardSchema);
-  } catch (error) {
-    console.log(error);
-  }
-  console.log('Target MongoDB connected.');
+    const targetModel = targetConn.model('Card', cardSchema);
+    console.log('MongoDB databases connected.');
 
-  return [sourceModel, targetModel];
-}
+    //
+    // build pipelines.
+    //
+    const pipeline = [];
+    console.log('Aggregating data from source database...');
 
-async function populateDatabaseFromURI(sourceURI, targetURI) {
-  console.log('Connecting to databases...');
-  const [sourceModel, targetModel] = await connectToDatabases(sourceURI, targetURI);
+    // match stage - filter non-playable cards.
+    const match = {
+      $match: {
+        set_type: {
+          $nin: [
+            'masters',
+            'masterpiece',
+            'from_the_vault',
+            'spellbook',
+            'premium_deck ',
+            'duel_deck',
+            'treasure_chest',
+            'planechase',
+            'archenemy',
+            'vanguard',
+            'funny',
+            'promo',
+            'token',
+            'memorabilia'
+          ]
+        },
+        layout: {
+          $nin: [
+            'planar',
+            'scheme',
+            'vanguard',
+            'token',
+            'double_faced_token',
+            'emblem',
+            'augment',
+            'host',
+            'art_series',
+            'double_sided'
+          ]
+        },
+        multiverse_ids: {
+          $ne: []
+        }
+      }
+    };
 
-  console.log('Building the pipelines...');
-  const pipeline = [];
-  // match stage - avoid cards that don't have a multiverse_id.
-  const match = {
-    $match: {
-      multiverse_ids: { $ne: [] }
-    }
-  };
-  // group stage - filter duplicate cards (keep the latest version of the card).
-  const group = {
-    $group: {
-      _id: '$oracle_id',
-      artist: { $first: '$artist' },
-      cmc: { $first: '$cmc' },
-      card_faces: { $first: '$card_faces' },
-      colors: { $first: '$colors' },
-      flavor_text: { $first: '$flavor_text' },
-      id: { $first: '$id' },
-      image_uris: { $first: '$image_uris' },
-      layout: { $first: '$layout' },
-      legalities: { $first: '$legalities' },
-      loyalty: { $first: '$loyalty' },
-      mana_cost: { $first: '$mana_cost' },
-      multiverse_ids: { $first: '$multiverse_ids' },
-      name: { $first: '$name' },
-      oracle_id: { $first: '$oracle_id' },
-      oracle_text: { $first: '$oracle_text' },
-      power: { $first: '$power' },
-      rarity: { $first: '$rarity' },
-      released_at: { $first: '$released_at' },
-      related_uris: { $first: '$related_uris' },
-      rulings_uri: { $first: '$rulings_uri' },
-      scryfall_uri: { $first: '$scryfall_uri' },
-      set: { $first: '$set' },
-      set_name: { $first: '$set_name' },
-      set_type: { $first: '$set_type' },
-      toughness: { $first: '$toughness' },
-      type_line: { $first: '$type_line' }
-    }
-  };
-  // project stage - filter _id field generated on group stage.
-  const project = {
-    $project: {
-      _id: 0,
-      'card_faces.artist_id': 0,
-      'card_faces.color_indicator': 0,
-      'card_faces.illustration_id': 0,
-      'card_faces.image_uris.art_crop': 0,
-      'card_faces.image_uris.border_crop': 0,
-      'card_faces.object': 0,
-      'card_faces.printed_name': 0,
-      'card_faces.printed_text': 0,
-      'card_faces.printed_type_line': 0,
-      'card_faces.watermark': 0,
-      'image_uris.art_crop': 0,
-      'image_uris.border_crop': 0,
-      'legalities.future': 0,
-      'legalities.historic': 0,
-      'legalities.penny': 0,
-      'legalities.duel': 0,
-      'legalities.oldschool': 0
-    }
-  };
-  pipeline.push(match);
-  pipeline.push(group);
-  pipeline.push(project);
+    // group stage - filter multiple versions of cards (keep the latest print of the card).
+    const group = {
+      $group: {
+        _id: '$oracle_id',
+        artist: { $first: '$artist' },
+        cmc: { $first: '$cmc' },
+        card_faces: { $first: '$card_faces' },
+        colors: { $first: '$colors' },
+        flavor_text: { $first: '$flavor_text' },
+        id: { $first: '$id' },
+        image_uris: { $first: '$image_uris' },
+        layout: { $first: '$layout' },
+        legalities: { $first: '$legalities' },
+        loyalty: { $first: '$loyalty' },
+        mana_cost: { $first: '$mana_cost' },
+        multiverse_ids: { $first: '$multiverse_ids' },
+        name: { $first: '$name' },
+        oracle_id: { $first: '$oracle_id' },
+        oracle_text: { $first: '$oracle_text' },
+        power: { $first: '$power' },
+        rarity: { $first: '$rarity' },
+        released_at: { $first: '$released_at' },
+        related_uris: { $first: '$related_uris' },
+        rulings_uri: { $first: '$rulings_uri' },
+        scryfall_uri: { $first: '$scryfall_uri' },
+        set: { $first: '$set' },
+        set_name: { $first: '$set_name' },
+        set_type: { $first: '$set_type' },
+        toughness: { $first: '$toughness' },
+        type_line: { $first: '$type_line' }
+      }
+    };
 
-  let results = [];
+    // project stage - filter useless fields generated on group stage.
+    const project = {
+      $project: {
+        _id: 0,
+        'card_faces.artist_id': 0,
+        'card_faces.color_indicator': 0,
+        'card_faces.illustration_id': 0,
+        'card_faces.image_uris.art_crop': 0,
+        'card_faces.image_uris.border_crop': 0,
+        'card_faces.object': 0,
+        'card_faces.printed_name': 0,
+        'card_faces.printed_text': 0,
+        'card_faces.printed_type_line': 0,
+        'card_faces.watermark': 0,
+        'image_uris.art_crop': 0,
+        'image_uris.border_crop': 0,
+        'legalities.future': 0,
+        'legalities.historic': 0,
+        'legalities.penny': 0,
+        'legalities.duel': 0,
+        'legalities.oldschool': 0
+      }
+    };
+    pipeline.push(match);
+    pipeline.push(group);
+    pipeline.push(project);
 
-  console.log('Aggregating data from source database...');
-  try {
-    results = await sourceModel.aggregate(pipeline);
-  } catch (error) {
-    console.log(error);
-  }
+    //
+    // aggregate data from source DB to insert it on target DB.
+    //
+    const results = await sourceModel.aggregate(pipeline);
+    console.log('Aggregated data ready.');
 
-  console.log('Inserting data into target database...');
-  try {
+    console.log('Inserting aggregated data into target database...');
     await targetModel.insertMany(results);
+
+    console.log('Succeed!');
   } catch (error) {
     console.log(error);
   }
-
-  console.log('Success!');
   process.exit([0]);
 }
 
-module.exports = populateDatabaseFromURI;
+populateDatabaseFromURI(process.env.SOURCE_DB_URI, process.env.TARGET_DB_URI);
